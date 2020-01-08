4f1668f928c06abff6f8edc14d1f6ed654ca9474
Lucas Souza <lucas-machado@outlook.com>
2020-01-08 21:29:08 +0000

    Desenvolvimento do modulo 9

--- PedidoVenda/src/main/java/bean/CadastroClienteBean.java
+++ PedidoVenda/src/main/java/bean/CadastroClienteBean.java
@@ -4,23 +4,29 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
 import model.Endereco;
+import service.NegocioException;
 
 @ManagedBean
 public class CadastroClienteBean {
 
-    private Endereco endereco;
-    
-    @PostConstruct
-    public void init() {
-	this.endereco = new Endereco();
-    }
+	private Endereco endereco;
 
-    public Endereco getEndereco() {
-        return endereco;
-    }
+	@PostConstruct
+	public void init() {
+		this.endereco = new Endereco();
+	}
 
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-    
+	public void salvar() {
+		throw new RuntimeException();
+		//throw new NegocioException("Método salvar ainda não foi implementado!");
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
 }
---
+++ PedidoVenda/src/main/java/service/NegocioException.java
@@ -0,0 +1,11 @@
+package service;
+
+public class NegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = 1719546889415406161L;
+
+	public NegocioException(String msg) {
+		super(msg);
+	}
+
+}
