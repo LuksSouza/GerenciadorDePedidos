794ebdcb1d8111c48de4bd2f94de4559bf7c90d7
Lucas Souza <lucas-machado@outlook.com>
2020-01-17 18:26:09 +0000

    Alterações com mapeamentos JPA

--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/ItemPedido.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/ItemPedido.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,9 +18,16 @@ public class ItemPedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private Integer quantidade;
     private BigDecimal valorUnitario;
+    
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
     private Pedido pedido;
+    
+    @OneToOne
+    @JoinColumn(name = "produto_id")
     private Produto produto;
 
 }
--- PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/Pedido.java
+++ PedidoVenda/src/main/java/com/algaworks/pedidovenda/model/Pedido.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.algaworks.pedidovenda.enumeration.FormaPagamento;
@@ -20,6 +23,7 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private Date dataCriacao;
 	private String observacao;
 	private Date DataEntrega;
@@ -28,9 +32,20 @@ public class Pedido {
 	private BigDecimal valorTotal;
 	private StatusPedido status;
 	private FormaPagamento formaPagamento;
+
+	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens;
+
+	@OneToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+
+	@OneToOne
+	@JoinColumn(name = "vendedor_id")
 	private Usuario vendedor;
+
+	@OneToOne
+	@JoinColumn(name = "endereco_entrega_id")
 	private EnderecoEntrega enderecoEntrega;
 
 }
